b99f4e1064969b6ab47fd9ed0b19139ca1986147
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-13 21:47:30 +0200

    initialized and completed the 100DoC day - 1

---
+++ day1-10/bandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band-name-generator\n")
+user_city = input("Enter the city you grew up in:\n")
+user_pet = input("Name one of your curent or previous pets:\n")
+
+print("Your newly generated band name is....\n\n" + user_city + " " + user_pet)
