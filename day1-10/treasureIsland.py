416cbad4d969a07d8c7a660755e79d0165c1a81e
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-15 22:46:09 +0200

    Checking if a year is a leap year using nested if statements

--- day1-10/treasureIsland.py
+++ day1-10/treasureIsland.py
@@ -1,20 +1,15 @@
 # Treasure Island
-input_height = float(input("What's your height? (In meters please)\n"))
-input_weight = int(input("What's your weight? (In kilograms please)\n"))
+input_year = int(input("What year want you check if it's a leap year?\n"))
 
-bmi = round(input_weight / input_height ** 2)
+year_divided = input_year / 4
 
-bmi = 36
-print(f"Your BMI is: {bmi}")
-
-if bmi < 18.5:
-    print("You are underweight")
-elif bmi > 18.5 and bmi < 35:
-    if bmi < 25:
-        print("You have a normal weight")
-    elif bmi > 25 and bmi < 30:
-        print("You are overweight")
+if input_year % 4 == 0:
+    if input_year % 100 == 0:
+        if input_year % 400 == 0:
+            print("Leap year!")
+        else:
+            print("Not a leap year!")
     else:
-        print("You are obese")
+            print("Leap year!")
 else:
-    print("You are clinically obese")
+    print("Not a leap year!")
