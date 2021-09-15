a8ed1bc38df54b66efe9e3bd305e42a67b180992
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-15 22:31:17 +0200

    used AND when comparing values of the BMI calculator

--- day1-10/treasureIsland.py
+++ day1-10/treasureIsland.py
@@ -1,9 +1,20 @@
 # Treasure Island
-input_number = int(input("What number would you want to check for odd or even?"))
+input_height = float(input("What's your height? (In meters please)\n"))
+input_weight = int(input("What's your weight? (In kilograms please)\n"))
 
-num_modulo = input_number % 2
+bmi = round(input_weight / input_height ** 2)
 
-if num_modulo == 0:
-    print("Even number")
-elif num_modulo == 1:
-    print("Odd number")
+bmi = 36
+print(f"Your BMI is: {bmi}")
+
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi > 18.5 and bmi < 35:
+    if bmi < 25:
+        print("You have a normal weight")
+    elif bmi > 25 and bmi < 30:
+        print("You are overweight")
+    else:
+        print("You are obese")
+else:
+    print("You are clinically obese")
