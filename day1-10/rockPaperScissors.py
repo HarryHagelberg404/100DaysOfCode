74a67ea8a3ba8cd0d52b4a5cedfc4ca45dd60504
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-16 23:54:19 +0200

    created a treasure map with nested lists

--- day1-10/rockPaperScissors.py
+++ day1-10/rockPaperScissors.py
@@ -1,7 +1,21 @@
 import random
 
-people_list = ["Krille", "Ogge", "Harry"]
+row_1 = ["X", "X", "X"]
 
-random_int = random.randint(0, 2)
+row_2 = ["X", "X", "X"]
 
-print(f"The person who is paying for dinner is: {people_list[random_int]}")
+row_3 = ["X", "X", "X"]
+
+map = [row_1, row_2, row_3]
+
+print(f"{row_1}\n{row_2}\n{row_3}")
+
+position = input("Where do you want to put the treasure? ex. 21 for 2 X and 1 Y")
+
+pos_x = position[1]
+
+pos_y = position[0]
+
+selected = map[int(pos_x) - 1][int(pos_y) - 1] = "O"
+
+print(f"{row_1}\n{row_2}\n{row_3}")
