50c5776293fb01e4b81d1e9785fbbb1aa706bbd4
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-17 19:23:36 +0200

    small exercise using for loops

--- day1-10/passwordGenerator.py
+++ day1-10/passwordGenerator.py
@@ -1,1 +1,14 @@
 # Password Generator
+fruits = input("Student heights spaced:\n").split()
+
+for n in range(0, len(fruits)):
+    fruits[n] = int(fruits[n])
+print(fruits)
+
+# calculate mean
+mean = 0
+for n in range(0, len(fruits)):
+    mean += fruits[n]
+
+mean /= len(fruits)
+print(f"Total mean of students:{mean}")
