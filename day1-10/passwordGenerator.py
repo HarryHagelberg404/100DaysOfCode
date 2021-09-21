3a88393b01ee0e979fbc9ea017cc201695289362
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-09-21 21:23:54 +0200

    refactoring file

--- day1-10/passwordGenerator.py
+++ day1-10/passwordGenerator.py
@@ -1,13 +1,2 @@
 # Password Generator
 
-highscores = input("Write a list of highscores separated by ' ':s down below:\n").split()
-
-top_highscore = 0
-for n in range(0, len(highscores)):
-    curr_score = int(highscores[n])
-    if curr_score > top_highscore:
-        top_highscore = curr_score
-
-print("The list of highscores:")
-print(highscores)
-print(f"The highest score:{top_highscore}")
