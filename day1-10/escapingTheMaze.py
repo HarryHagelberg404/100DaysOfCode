52f398ac1567ff102e775579b2f4e0deccd751ca
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-10-07 20:54:12 +0200

    completed the challenge of escaping the maze

--- day1-10/escapingTheMaze.py
+++ day1-10/escapingTheMaze.py
@@ -1,1 +1,20 @@
 # Escaping the maze
+# The code that was written inside the website
+# NOTE. that theese are predefined functions inside the website.
+# NOTE. that this code does not take regard to the edge case of when the robot is spawned in a void containing no wall to the right.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+
