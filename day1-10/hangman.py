6fa13c687067354d19655926b1b80d35b110f8f9
HarryHagelberg404 <harryhagelberg99@gmail.com>
2021-10-12 20:24:31 +0200

    wrote psuedo code for todays hangman challenge

---
+++ day1-10/hangman.py
@@ -0,0 +1,16 @@
+# Hangman psuedo code:
+# Generate a random word
+# Generate as many blanks as letters in the word
+# The user guesses a letter
+# Is the guessed letter in the word? Yes / No
+
+# Y Replace the blank with the letter
+# Y Are all blanks filled?
+#   Y GAME OVER
+#   N The user keeps guessing
+
+# N Lose a life
+# N Have the user run out of lives?
+#   Y GAME OVER
+#   N The user keeps guessing
+
